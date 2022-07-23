36089cb778bb01ba5c0de9da3a311853ef9532e0
ansango <anibalsantosgo@gmail.com>
2022-07-23 20:41:25 +0200

    change cards

--- components/pages/dashboard/profile/Themes.tsx
+++ components/pages/dashboard/profile/Themes.tsx
@@ -10,54 +10,56 @@ const Themes = () => {
   useEffect(() => setMounted(true), []);
 
   return (
-    <CardSlim>
-      <Step
-        title="Temas"
-        step={1}
-        icon={{
-          kind: "outline",
-          type: "ColorSwatchIcon",
-        }}
-      >
-        <div className="grid gap-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 py-2">
-          {mounted &&
-            themes.map((theme) => {
-              return (
-                <div
-                  key={theme}
-                  className={
-                    theme === currentTheme
-                      ? "outline-primary overflow-hidden rounded-lg outline-4 outline-offset-2 outline"
-                      : "outline-base-content overflow-hidden rounded-lg outline-2 outline-offset-2"
-                  }
-                  data-set-theme={theme}
-                  data-act-class="outline"
-                  onClick={() => setTheme(theme)}
-                >
+    <div className="card card-compact overflow-visible w-full image-full opacity-90 glass shadow-md bg-secondary-focus hover:bg-secondary-focus text-secondary-content">
+      <div className="card-body">
+        <Step
+          title="Temas"
+          step={1}
+          icon={{
+            kind: "outline",
+            type: "ColorSwatchIcon",
+          }}
+        >
+          <div className="grid gap-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 py-2">
+            {mounted &&
+              themes.map((theme) => {
+                return (
                   <div
-                    data-theme={theme}
-                    className="bg-base-100 text-base-content w-full cursor-pointer font-sans"
+                    key={theme}
+                    className={
+                      theme === currentTheme
+                        ? "outline-primary overflow-hidden rounded-lg outline-4 outline-offset-2 outline"
+                        : "outline-base-content overflow-hidden rounded-lg outline-2 outline-offset-2"
+                    }
+                    data-set-theme={theme}
+                    data-act-class="outline"
+                    onClick={() => setTheme(theme)}
                   >
-                    <div className="grid grid-cols-5 grid-rows-3">
-                      <div className="col-span-5 row-span-3 row-start-1 flex gap-1 py-3 px-4">
-                        <div className="flex-grow text-sm font-bold capitalize">
-                          {theme}
-                        </div>
-                        <div className="flex flex-shrink-0 flex-wrap gap-1">
-                          <div className="bg-primary w-2 rounded"></div>
-                          <div className="bg-secondary w-2 rounded"></div>
-                          <div className="bg-accent w-2 rounded"></div>
-                          <div className="bg-neutral w-2 rounded"></div>
+                    <div
+                      data-theme={theme}
+                      className="bg-base-100 text-base-content w-full cursor-pointer font-sans"
+                    >
+                      <div className="grid grid-cols-5 grid-rows-3">
+                        <div className="col-span-5 row-span-3 row-start-1 flex gap-1 py-3 px-4">
+                          <div className="flex-grow text-sm font-bold capitalize">
+                            {theme}
+                          </div>
+                          <div className="flex flex-shrink-0 flex-wrap gap-1">
+                            <div className="bg-primary w-2 rounded"></div>
+                            <div className="bg-secondary w-2 rounded"></div>
+                            <div className="bg-accent w-2 rounded"></div>
+                            <div className="bg-neutral w-2 rounded"></div>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
-        </div>
-      </Step>
-    </CardSlim>
+                );
+              })}
+          </div>
+        </Step>
+      </div>
+    </div>
   );
 };
 
